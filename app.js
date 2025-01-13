8e4576b8f4cef38237eefee8d11b4c4d1d6ea7cd
Mahesh Kumar Kamalakar <maheshkamalakar1@gmail.com>
2025-01-13 13:17:52 +0530

    added mistake in this file

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //this is for adding new features to the app
-<<<<<<< HEAD
+
 // added new feature - button
-=======
+
 // add new feature - button
 // add new feature - input
->>>>>>> features
+
+//mistakes
+
+
